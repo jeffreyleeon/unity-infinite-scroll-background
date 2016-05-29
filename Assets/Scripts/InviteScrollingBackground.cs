706689bf273b1aefe680e8fa15250f2fb83014c5
Jeffrey Lee <jeffreyleeon@gmail.com>
2016-05-29 12:17:22 +0800

    First Commit

---
+++ Assets/Scripts/InviteScrollingBackground.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InviteScrollingBackground : MonoBehaviour {
+
+	public float speed = 5.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 offset = new Vector2 (Time.time * speed, 0);
+		Renderer renderer = GetComponent<Renderer> ();
+		renderer.material.mainTextureOffset = offset;
+	}
+}
